aef2a5a1671d2edfba7a9f2d77c3ccc02ee9c4dc
GIL32610 <gilaraujo417@gmail.com>
2025-10-23 11:48:40 +0000

    add keras weight extractor example

---
+++ stegocnn/examples/weight_extractor_example.py
@@ -0,0 +1,8 @@
+from utils import WeightExtractor
+from utils import ModelLoader
+
+if __name__ == '__main__':
+    output_path = '../outputs/keras/weights'
+    model_path = '../outputs/keras/S-UNIWARD_0.2bpp.hdf5'
+    model = ModelLoader.load_tensorflow_model(model_path=model_path)
+    WeightExtractor.extract_weights_from_keras(model=model, output_path=output_path)
--- stegocnn/utils.py
+++ stegocnn/utils.py
@@ -1,5 +1,6 @@
 from torchvision import transforms 
 import torch
+import os
 import numpy as np
 import tensorflow as tf
 
@@ -13,4 +14,14 @@ class ModelLoader:
     @staticmethod
     def load_tensorflow_model(model_path) -> tf.keras.Model:
         return tf.keras.models.load_model(filepath=model_path, compile=False, custom_objects={'Tanh3': lambda x: 3 * tf.keras.activations.tanh(x)})
-        
+    
+class WeightExtractor:
+    @staticmethod
+    def extract_weights_from_keras(model:tf.keras.Model, output_path):
+        model_name = model.name
+        for i, layer in enumerate(model.layers):
+            filename = f'{layer.name}_{i}_weights.npy' 
+            output_path = os.path.join(output_path, model_name, filename)
+            weights = layer.get_weights()
+            np.save(file=output_path,arr=weights)
+            print(f'Saved {layer.name}')
