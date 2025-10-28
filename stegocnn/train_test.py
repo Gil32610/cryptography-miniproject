1497279a60acbcff7f355e42a70245f0d5d9b932
JoaoGabrielTN <joaogabrieltdonascimento@gmail.com>
2025-10-28 11:22:41 -0300

    treino do modelo student corrigido

--- stegocnn/train_test.py
+++ stegocnn/train_test.py
@@ -121,38 +121,62 @@ def train_pytorch(model, X_train, y_train, X_valid, y_valid, X_test, y_test, bat
     
     return {"test_loss": final_test_loss, "test_accuracy": final_test_accuracy}
 
-def train_knowledge_distillation(teacher, student, train_loader, epochs, learning_rate, T, soft_target_loss_weight, ce_loss_weight, device):
+def train_knowledge_distillation(teacher, student, X_train, y_train, X_valid, y_valid, X_test, y_test, epochs, learning_rate, T, soft_target_loss_weight, ce_loss_weight, device, model_name="", path_log_base="logs", batch_size=32):
+    start_time = tm.time()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    student.to(device)
+
+    X_train_t = torch.tensor(X_train, dtype=torch.float32)
+    y_train_t = torch.tensor(y_train, dtype=torch.long) # Use torch.long for classification labels
+    X_valid_t = torch.tensor(X_valid, dtype=torch.float32)
+    y_valid_t = torch.tensor(y_valid, dtype=torch.long)
+    X_test_t  = torch.tensor(X_test, dtype=torch.float32)
+    y_test_t  = torch.tensor(y_test, dtype=torch.long)
+
+    train_dataset = TensorDataset(X_train_t, y_train_t)
+    valid_dataset = TensorDataset(X_valid_t, y_valid_t)
+    test_dataset  = TensorDataset(X_test_t, y_test_t)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    valid_loader = DataLoader(valid_dataset, batch_size=batch_size)
+    test_loader  = DataLoader(test_dataset, batch_size=batch_size)
+
     ce_loss = nn.CrossEntropyLoss()
     optimizer = optim.Adam(student.parameters(), lr=learning_rate)
 
-    teacher.eval()  # Teacher set to evaluation mode
-    student.train() # Student to train mode
+    log_timestamp = datetime.datetime.now().isoformat()[:19].replace("T", "_").replace(":", "-")
+    log_dir = os.path.join(path_log_base, f"{model_name}_{log_timestamp}")
+    os.makedirs(log_dir, exist_ok=True)
+    writer = SummaryWriter(log_dir)
 
+    lossTEST, accuracyTEST = evaluate_model(student, test_loader, ce_loss, device)
+    lossTRAIN, accuracyTRAIN = evaluate_model(student, train_loader, ce_loss, device)
+    lossVALID, accuracyVALID = evaluate_model(student, valid_loader, ce_loss, device)
+
+    teacher.eval()  # Teacher set to evaluation mode
+
+    print(f"Initial Metrics - Train Loss: {lossTRAIN:.4f}, Acc: {accuracyTRAIN:.4f} | Valid Loss: {lossVALID:.4f}, Acc: {accuracyVALID:.4f} | Test Loss: {lossTEST:.4f}, Acc: {accuracyTEST:.4f}")
+    print("Starting the training...")
     for epoch in range(epochs):
+        student.train() # Student to train mode
         running_loss = 0.0
         for inputs, labels in train_loader:
             inputs, labels = inputs.to(device), labels.to(device)
 
             optimizer.zero_grad()
 
-            # Forward pass with the teacher model - do not save gradients here as we do not change the teacher's weights
             with torch.no_grad():
                 teacher_logits = teacher(inputs)
 
-            # Forward pass with the student model
             student_logits = student(inputs)
 
-            #Soften the student logits by applying softmax first and log() second
-            soft_targets = nn.functional.softmax(teacher_logits / T, dim=-1)
-            soft_prob = nn.functional.log_softmax(student_logits / T, dim=-1)
+            soft_targets = nn.functional.softmax(teacher_logits / T, dim=-1) # maybe 1? João
+            soft_prob = nn.functional.log_softmax(student_logits / T, dim=-1) # maybe 1? João
 
-            # Calculate the soft targets loss. Scaled by T**2 as suggested by the authors of the paper "Distilling the knowledge in a neural network"
             soft_targets_loss = torch.sum(soft_targets * (soft_targets.log() - soft_prob)) / soft_prob.size()[0] * (T**2)
 
-            # Calculate the true label loss
             label_loss = ce_loss(student_logits, labels)
 
-            # Weighted sum of the two losses
             loss = soft_target_loss_weight * soft_targets_loss + ce_loss_weight * label_loss
 
             loss.backward()
@@ -160,4 +184,28 @@ def train_knowledge_distillation(teacher, student, train_loader, epochs, learnin
 
             running_loss += loss.item()
 
+        train_loss = running_loss / len(train_loader)
+        
+        val_loss, val_accuracy = evaluate_model(student, valid_loader, ce_loss, device)
+        
+        print(f"Epoch {epoch:03d}/{epochs} - Loss: {train_loss:.4f} - val_loss: {val_loss:.4f} - val_accuracy: {val_accuracy:.4f}")
+
+        writer.add_scalar('Loss/train', train_loss, epoch)
+        writer.add_scalar('Loss/valid', val_loss, epoch)
+        writer.add_scalar('Accuracy/valid', val_accuracy, epoch)
+        
+        checkpoint_path = os.path.join(log_dir, f"saved-model-{epoch:03d}-{val_accuracy:.4f}.pth")
+        torch.save(student.state_dict(), checkpoint_path)
+
         print(f"Epoch {epoch+1}/{epochs}, Loss: {running_loss / len(train_loader)}")
+    
+     writer.close()
+    
+    final_test_loss, final_test_accuracy = evaluate_model(student, test_loader, ce_loss, device)
+    
+    TIME = tm.time() - start_time
+    print(f"Time {model_name} = {TIME:.2f} [seconds]")
+    print("\n")
+    print(log_dir)
+    
+    return {"test_loss": final_test_loss, "test_accuracy": final_test_accuracy}
