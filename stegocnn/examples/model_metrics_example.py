b3b911e545fde7dd31e71474b18d6caa218ef794
GIL32610 <gil.00000032610@unicap.br>
2025-10-31 10:22:31 -0300

    add metrics example

--- stegocnn/examples/model_metrics_example.py
+++ stegocnn/examples/model_metrics_example.py
@@ -1,4 +1,4 @@
-from models import GBRASNET
+from models import GBRASNETStudent, GBRASNET
 from analysis.plot import Plotter
 from images.dataset import PGMImageDataset
 import torch
@@ -7,18 +7,19 @@ if __name__ == '__main__':
     dataset = PGMImageDataset(
     cover_path='../data/BOSSbase-1.01/cover',
     stego_path='../data/BOSSbase-1.01/stego',
-    stego_algorithm='WOW',
-    bpp='0.4bpp',
+    stego_algorithm='S-UNIWARD',
+    bpp='0.2bpp',
     test=True
     )
     plotter = Plotter(dataset=dataset)
     model = GBRASNET(srm_path='../data/kernels/SRM_Kernels1.npy')
-    trained_model_path = '../data/outputs/torch/WOW_ORGIGINAL/.4bpp/saved-model-034-0.7645.pth'
+    trained_model_path = '../data/outputs/torch/Weight Decay/S-UNIWARD/.2bpp/_2025-10-31_02-24-08/saved-model-031-0.6295.pth'
     state_dict = torch.load(f=trained_model_path)
+    print(state_dict.keys())
     model.load_state_dict(state_dict=state_dict)
     metrics = plotter.get_metrics_results(model=model,batch_size=4)
     print(metrics)
-    model_name = 'WOW 4bpp original model'
+    model_name = 'S-UNIWARD 2bpp weight decay model'
     plotter.plot_confusion_matrix(save_path='../data/outputs/plots',model_name=model_name)
     plotter.plot_roc_curve(save_path='../data/outputs/plots',model_name=model_name)
     
