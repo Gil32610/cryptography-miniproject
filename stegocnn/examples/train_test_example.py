0625fc2d6d0029443073ff0d2bb9d29813c6abf7
Gil32610 <99660917+Gil32610@users.noreply.github.com>
2026-02-28 20:38:29 -0300

    Feature/regularization (#9)
    * Fixed a small error in train_test
    
    * Added weight decay regularization and Docker adjustments
    
    ---------
    
    Co-authored-by: HeloisaJ <heloisananda21@gmail.com>

--- stegocnn/examples/train_test_example.py
+++ stegocnn/examples/train_test_example.py
@@ -6,10 +6,10 @@ import pdb
 if __name__ == '__main__':
     cover_path='../data/BOSSbase-1.01/cover'
     stego_path='../data/BOSSbase-1.01/stego'
-    stego_algorithm = 'HILL'
-    dataset_train = PGMImageDataset(cover_path=cover_path, stego_path=stego_path, stego_algorithm=stego_algorithm)
-    dataset_val = PGMImageDataset(cover_path=cover_path, stego_path=stego_path, stego_algorithm=stego_algorithm, val=True)
-    dataset_test = PGMImageDataset(cover_path=cover_path, stego_path=stego_path, stego_algorithm=stego_algorithm, test=True)
+    stego_algorithm = 'S-UNIWARD'
+    dataset_train = PGMImageDataset(cover_path=cover_path, stego_path=stego_path, stego_algorithm=stego_algorithm, bpp="0.4bpp")
+    dataset_val = PGMImageDataset(cover_path=cover_path, stego_path=stego_path, stego_algorithm=stego_algorithm, val=True, bpp="0.4bpp")
+    dataset_test = PGMImageDataset(cover_path=cover_path, stego_path=stego_path, stego_algorithm=stego_algorithm, test=True, bpp="0.4bpp")
     srm_path = '../data/kernels/SRM_Kernels1.npy'
     model = GBRASNET(srm_path=srm_path)
-    train_pytorch(model=model,dataset_train=dataset_train, dataset_val=dataset_val, dataset_test=dataset_test, batch_size=16)
+    train_pytorch(model=model,dataset_train=dataset_train, dataset_val=dataset_val, dataset_test=dataset_test, batch_size=16, epochs=100, path_log_base="../data/outputs/torch/decay/S-UNIWARD_.4bpp", weight_decay=True)
